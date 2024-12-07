e174fb4fd5d45d5d8faa97499a709d381037f156
ziko <zkariaelkhaldi@gmail.com>
2024-12-07 22:17:35 +0100

    second commit

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,2 +1,19 @@
+return {
+  { "morhetz/gruvbox" },
+  { "sainnhe/gruvbox-material" },
+  { "arcticicestudio/nord" },
+  { "dracula/vim" },
+  { "joshdick/onedark.vim" },
+  { "rose-pine/neovim" },
+  { "mellow-theme/mellow.nvim" },
+  { "javiorfo/nvim-nyctophilia" },
+  { "bettervim/yugen.nvim" },
+  { "aktersnurra/no-clown-fiesta.nvim",
 
-return{}
+    config = function()
+      vim.cmd([[colorscheme gruvbox-material]])
+    end
+  },
+}
+
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,2 +1,52 @@
-
-return{}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "c",
+          "cpp",
+          "lua",
+          "python",
+          "javascript",
+          "html",
+          "css",
+          "bash",
+          "json",
+          "yaml",
+          "markdown",
+        },
+        highlight = {
+          enable = true, -- Enable syntax highlighting
+          additional_vim_regex_highlighting = false, -- Avoid conflict with Vim's built-in highlighting
+        },
+        indent = {
+          enable = true, -- Enable Treesitter-based indentation
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<CR>",
+            node_incremental = "<CR>",
+            scope_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
